cf70aba824bf424758c3496d8609f4cf115f2a51
mark-torres-10 <markptorres1@gmail.com>
2020-12-28 15:35:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+ */
+
+/* 
+Get user value from input and save it to variable
+*/
+
+/*
+Generate a random number 1-100, for a person to guess
+*/
+
+/*
+Compare the user's guess to the generated number
+*/
+
+/*
+Display the result ("Guess again" if you're off, "Congrats!" if you're right)
+*/
+
+/* 
+Wrapper function to play the game
+*/
+
+let playGame = () => {
+  console.log("Play me!");
+};
