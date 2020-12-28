317eb34305b0d54a5ddaee13ce1af69e3215b4c1
mark-torres-10 <markptorres1@gmail.com>
2020-12-28 18:21:05 -0500

    fixing UI

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ Generate a random number 1-100, for a person to guess
 Compare the user's guess to the generated number
 */
 
+let generateValue = () => {
+  let randomFloat = Math.random() * 100;
+  let randomInt = Math.floor(randomFloat);
+  return randomInt;
+};
+
+randomNumber = generateValue();
+console.log(`Our random number is: ${randomNumber}`);
+
 /*
 Display the result ("Guess again" if you're off, "Congrats!" if you're right)
 */
